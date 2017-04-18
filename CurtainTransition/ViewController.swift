8af55b4e2d8b071d744be8c432178286eed8b7e0
waleed azhar <waleedazhar@waleeds-mbp.lan>
2017-04-18 00:18:42 -0400

    Initial Commit

---
+++ CurtainTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurtainTransition
+//
+//  Created by waleed azhar on 2017-04-18.
+//  Copyright © 2017 waleed azhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
